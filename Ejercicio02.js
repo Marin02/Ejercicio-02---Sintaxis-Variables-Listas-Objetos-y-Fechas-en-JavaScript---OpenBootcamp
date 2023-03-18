ba66e1d25c4440820ea496a1e95b7815f9bfc695
Juan Felipe Marin <102315395+Marin02@users.noreply.github.com>
2023-03-17 22:35:07 -0500

    Add files via upload

---
+++ Ejercicio02.js
@@ -0,0 +1,17 @@
+// Ejercicio 02
+
+let ar = [
+    "Felipe Marin",
+    19,
+    true,
+    new Date(2004, 1, 2),
+    libroFav = {
+        titulo: 'El Psicoanalista',
+        autor: 'John Katzenbach',
+        fecha: '29 de enero de 2002',
+        url: 'https://es.wikipedia.org/wiki/El_psicoanalista'
+    }
+];
+
+console.log(ar);
+
